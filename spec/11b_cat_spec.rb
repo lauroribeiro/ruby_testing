90f599ff40fb186d0e98291d6fa495c6cbcd990a
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-08-01 20:29:14 -0300

    aswer question

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:laboon) { described_class.new('Laboon', 'SRD') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat is a child Class of pet' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(laboon).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(laboon).to be_hiding
   end
 end
